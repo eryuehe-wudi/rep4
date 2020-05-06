466ee63d26f0beb10198c10412628fdf3e990f11
zahngsan <zhang@itcast.cn>
2020-05-06 22:41:47 +0800

    忽略文件

--- day13_git/src/com/itheima/demo01/HelloWorld.java
+++ day13_git/src/com/itheima/demo01/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.itheima.demo01;
 
+//我是一个老屌丝
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("我下班了，代码要提交到git");
     }
 }
